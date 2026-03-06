14e2c6175a08cf57de65f9aa4e6b0afb0ebff1a8
Logan-Paul01 <daramolaoluwanifemi0@gmail.com>
2026-03-06 18:17:19 +0100

    Updated streamlit_interface_forEksu_Course_Risk_Predictor.py

--- streamlit_interface_forEksu_Course_Risk_Predictor.py
+++ streamlit_interface_forEksu_Course_Risk_Predictor.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 import os
 
-st.title("EKSU Course Risk Predictor")
+st.title("Eksu Student Course Risk Predictor")
 
 # Load model safely
 MODEL_PATH = "eksu_risk_model.joblib"
@@ -13,14 +13,22 @@ if not os.path.exists(MODEL_PATH):
 else:
     model = joblib.load(MODEL_PATH)
 
-    study = st.slider("Study hours per week", 0, 40, 10)
-    sleep = st.slider("Sleep hours per day", 3, 10, 6)
-    phone = st.slider("Phone usage hours per day", 0, 12, 5)
+    study = st.slider("Study hours per week",0,40,10)
+    
+    sleep = st.slider("Avg Sleep hours",4,10,6)
+    
+    phone = st.slider("Phone usage hours",0,12,5)
+    
+    attendance = st.slider("Attendance level (1=poor, 5=excellent)",1,5,3)
+    
+    difficulty = st.slider("Course difficulty (1=easy, 5=hard)",1,5,3)
+    
+    stress = st.slider("Financial stress (1=none, 5=extreme)",1,5,3)
 
-    attendance = st.selectbox("Attendance level", [0,1,2])
-    difficulty = st.selectbox("Course difficulty", [0,1,2])
-    stress = st.selectbox("Financial stress", [0,1,2])
+    gpa = st.input("Last semester GPA")
 
+    st.body("If you are a 100l students kindly input 3.00 as your last semester gpa as to not skew your prediction")
+    
     if st.button("Analyze Risk"):
 
         X = np.array([[study, sleep, phone, attendance, difficulty, stress]])
@@ -29,7 +37,8 @@ else:
 
         st.subheader("Failure Probability")
 
-        st.write(prob)
+        proba = (prob * 100)
+        st.write(proba)
 
         if prob < 0.3:
             st.success("Low Risk")
@@ -37,3 +46,5 @@ else:
             st.warning("Moderate Risk")
         else:
             st.error("High Risk")
+
+st.subtitle("Dear Students Your inputed data is used to improve the model anonymously, this if for educational purposes only and should not be heavily relied upon")
